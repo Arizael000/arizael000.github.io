d4bd596d24ec36af80121a8cd524a24da67018d3
Arizael000 <arizogurdian@gmail.com>
2024-01-26 05:46:50 -0500

    Fixing responsive stuff

--- js/darkmode.js
+++ js/darkmode.js
@@ -1,39 +1,58 @@
 
 document.addEventListener('DOMContentLoaded', function() {
-    const darkModeToggle = document.getElementById('darkModeToggle');
+const darkModeToggle = document.getElementById('darkModeToggle');
 const body = document.body;
 const isDarkMode = localStorage.getItem('darkMode') === 'enabled';
 const cuadro=document.getElementById('cuadro');
-const divBorde=document.getElementById('divBorde');
-/*var spans= document.querySelectorAll('.socialName');
-const span= document.querySelector('.social-container .item span:last-child');*/
+const header=document.getElementById("container");
+const nav=document.getElementById("navbar");
+const moon=document.getElementById("moon");
+const socialnames=document.getElementsByClassName("socialName");
 if (isDarkMode) {
     body.classList.add('dark-mode');
     darkModeToggle.checked = true;
-    /*divBorde.style.boxShadow: "none";*/
     cuadro.classList.add('dark-mode');
+    header.classList.add('dark-mode');
+    nav.classList.add('dark-mode');
+    moon.style.setProperty('color' ,'#f9532d');
+    for(let i=0; i<socialnames.length; i++){
+        socialnames[i].style.setProperty('color' ,'white')
+    }
+} else {
+        body.classList.remove('dark-mode');
+        cuadro.classList.remove('dark-mode');
+        header.classList.remove('dark-mode');
+        nav.classList.remove('dark-mode');
+        moon.classList.remove('dark-mode');
+        moon.style.setProperty('color' ,'#1F252E');
+        for(let i=0; i<socialnames.length; i++){
+            socialnames[i].style.setProperty('color' ,'black')
+        }
+
 }
+
 darkModeToggle.addEventListener('change', () => {
     if (darkModeToggle.checked) {
         body.classList.add('dark-mode');
         cuadro.classList.add('dark-mode');
-        /*divBorde.style.boxShadow= "none";*/
+        header.classList.add('dark-mode');
+        nav.classList.add('dark-mode');
+        moon.style.setProperty('color' ,'#f9532d');
         localStorage.setItem('darkMode', 'enabled');
-        /*spans.forEach(element => {
-            element.classList.remove('dark-mode');
-        });*/
-       
-        
-        
+        for(let i=0; i<socialnames.length; i++){
+            socialnames[i].style.setProperty('color' ,'white')
+        }
     } else {
         body.classList.remove('dark-mode');
         cuadro.classList.remove('dark-mode');
-        /*divBorde.style.boxShadow= "5px 5px 15px #D1D9E6, -5px -5px 15px #ffffff";*/
-        /*spans.forEach(element => {
-            element.classList.add('dark-mode');
-        });*/
-        /* span.style.color='black';*/
+        header.classList.remove('dark-mode');
+        nav.classList.remove('dark-mode');
+        moon.classList.remove('dark-mode');
+        moon.style.setProperty('color' ,'#1F252E');
         localStorage.setItem('darkMode', 'disabled');
+        for(let i=0; i<socialnames.length; i++){
+            socialnames[i].style.setProperty('color' ,'black')
+        }
     }
 });
-  });
+});
