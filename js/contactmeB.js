a518613356c4b25109b5fd22a58d666b52dce238
Arizael000 <arizogurdian@gmail.com>
2024-01-27 02:29:55 -0500

    Added a form for send email

--- js/contactmeB.js
+++ js/contactmeB.js
@@ -1,10 +1,27 @@
 
 
 
-document.addEventListener('DOMContentLoaded', function() {
+/*document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('scrollUp').addEventListener('click', function(){
       setTimeout(function() {
         window.scrollTo({top: 0, behavior: 'smooth'});
       }, 300); // Agregar un retraso de 1 segundo antes de llamar a la función scrollTo
     });
-  });
+  });*/
+
+  // Funciones para abrir y cerrar el modal
+function openModal() {
+  document.getElementById('myModal').style.display = 'block';
+}
+
+function closeModal() {
+  document.getElementById('myModal').style.display = 'none';
+}
+
+// Cierra el modal si se hace clic fuera de él
+window.onclick = function(event) {
+  var modal = document.getElementById('myModal');
+  if (event.target == modal) {
+      modal.style.display = 'none';
+  }
+}
