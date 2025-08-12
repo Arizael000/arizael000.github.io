7b34f139226eb1a24e41868230383449a3483982
Arizael000 <arizogurdian@gmail.com>
2025-08-12 14:36:31 -0600

    Updated cards 2 and 3

--- js/enlaces.js
+++ js/enlaces.js
@@ -1,6 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
     var qrscanProject = document.getElementById('qrScanProject');
     var logisticPredictionProject = document.getElementById('logisticPredictionProject');
+    var hotelProject = document.getElementById('hotelProject');
+    var qgisParameters = document.getElementById('qgisParameters');
     if(qrscanProject){
         qrscanProject.addEventListener("click", function(e){
             e.preventDefault(); // Previene el salto por el href
@@ -11,6 +13,19 @@ document.addEventListener('DOMContentLoaded', function() {
         logisticPredictionProject.addEventListener("click", function(e){
             e.preventDefault(); // Previene el salto por el href
             window.open("https://github.com/Arizael000/logisticPrediction", "_blank");
+        });
+    }
+
+    if(hotelProject){
+        hotelProject.addEventListener("click", function(e){
+            e.preventDefault(); // Previene el salto por el href
+            window.open("https://github.com/Arizael000/SG_Hotel", "_blank");
+        });
+    }
+    if(qgisParameters){
+        qgisParameters.addEventListener("click", function(e){
+            e.preventDefault(); // Previene el salto por el href
+            window.open("https://github.com/Arizael000/Parameters-Extraction-in-QGIS", "_blank");
         });
     }
 });
